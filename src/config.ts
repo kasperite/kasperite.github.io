a1b8745d63e9e3a277396602a4c55613db5c72f3
Lukas Nguyen <lukas.nguyen9288@gmail.com>
2026-02-06 21:24:08 +1300

    feat: rebuild personal site in astro

---
+++ src/config.ts
@@ -0,0 +1,98 @@
+export const siteConfig = {
+  name: "Lukas Nguyen",
+  title: "Senior Software Engineer",
+  description: "Portfolio website of Lukas Nguyen",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "lukas.nguyen9288@@gmail.com",
+    linkedin: "https://www.linkedin.com/in/lukasquangnguyen/",
+    github: "https://github.com/kasperite",
+    twitter: "",
+    stackoverflow: "http://stackoverflow.com/users/1253504/kasperite",
+  },
+  aboutMe:  
+    `I have over 12 years of experience delivering government and commercial projects across Australia and New Zealand.
+    If you are looking for development support or would like to connect, feel free to reach out via email. I am based in New Zealand and open to remote opportunities.
+    `,
+  skills: ["Ruby", "Python", "Javascript", "React", "Vue", "Node.js",  "AWS", "Docker"],
+  projects: [
+    // {
+    //   name: "something",
+    //   description:
+    //     "something",
+    //   link: "something",
+    //   skills: ["React", "Node.js", "AWS"],
+    // },
+  ],
+  experience: [
+    {
+      title: "Freelancer (Remote)",
+      dateRange: "June 2023 - Present",
+      bullets: [
+        "Delivered ongoing website retainer services for a diverse portfolio of clients."
+      ],
+    },
+    {
+      company: "Marketplacer",
+      title: "Senior Software Engineer (Remote)",
+      dateRange: "Mar 2022 - December 2025",
+      bullets: [
+        "Maintained and developed new features for the Marketplacer admin dashboard, enabling sellers to manage their stores and update content efficiently.",
+        "Containerized the test environment, significantly simplifying deployments and reducing environment-related inconsistencies.",
+        "Reduced oversized API requests by 20%, improving system performance by enforcing strict page size limits.",
+        "Optimized the CI/CD pipeline by significantly reducing asset compilation time and improving build throughput."
+      ],
+    },
+    {
+      company: "Loyalty New Zealand",
+      title: "Senior Software Engineer",
+      dateRange: "Mar 2019 - Mar 2022",
+      bullets: [
+        "Designed, built, and maintained an internal dashboard for analysts to manage marketing metadata and configuration.",
+        "Developed and maintained robust microservices for the Loyalty platform utilising an array of technologies, including Ruby, Java, and Elixir, to enhance system functionality and user experience",
+        "Collaborated with the architecture practice team to establish strategic roadmaps and define architectural principles, providing valuable insights to support the wider technology team in achieving project goals.",
+      ],
+    },
+    {
+      company: "Ackama",
+      title: "Developer / Senior Developer",
+      dateRange: "Sep 2016 - Mar 2019",
+      bullets: [
+        "Contracted at New Zealand Post to enhance the parcel tracking API, enabling seamless integration with multiple backend systems",
+        "Built and maintained applications for clients across government and commercial sectors."
+      ],
+    },
+    {
+      company: "Radio New Zealand",
+      title: "Digital Developer",
+      dateRange: "Oct 2015 - Aug 2016",
+      bullets: [
+        "Contributed to the redesign of Radio New Zealand’s flagship website as part of a specialist delivery team.",
+        "Built data visualisation for various internal projects"
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "University of Canterbury",
+      degree: "Bachelor of Commerce major in Information System",
+      dateRange: "2007 - 2010",
+      achievements: [
+      ],
+    },
+    {
+      school: "Amazon Web Service",
+      degree: "AWS Certified Solutions Architect - Associate",
+      dateRange: "2020",
+      achievements: [
+      ],
+    },
+    {
+      school: "Amazon Web Service",
+      degree: "AWS Certified Developer - Associate",
+      dateRange: "2017",
+      achievements: [
+      ],
+    },
+  ],
+};
